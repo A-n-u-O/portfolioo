1852eb919d29f61a54a6dbea70888ceeed4f79e8
Anuoluwapo <anuvictor2005@gmail.com>
2025-07-10 04:47:43 +0100

    folder structure set up

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Anuoluwapo | Developer Portfolio",
+  description: "Frontend Developer Portfolio",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-black text-white`}>
+        {children}
+      </body>
+    </html>
+  );
+}
